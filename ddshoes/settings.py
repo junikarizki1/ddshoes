6e757453592e46dd38cf0243f9f98e5d46d7e300
junikarizki1 <rizkijrstmik22@gmail.com>
2026-05-18 23:32:01 +0700

    Fix ALLOWED_HOSTS to include Railway domains

--- ddshoes/settings.py
+++ ddshoes/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-qjl7*cx^#v*fq8@a3=&vr
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS += ['.railway.app', '.ngrok-free.dev']
 
 
 # Application definition
