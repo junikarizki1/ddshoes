db30d055587e4bd9d0f9d733497d1682839e7b4b
junikarizki1 <rizkijrstmik22@gmail.com>
2026-05-18 22:42:41 +0700

    Fix Railway 2

--- ddshoes/settings.py
+++ ddshoes/settings.py
@@ -152,11 +152,11 @@ WSGI_APPLICATION = 'ddshoes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ddshoes',     
-        'USER': 'postgres',       
-        'PASSWORD': 'root', 
-        'HOST': 'localhost',       
-        'PORT': '5432',  
+        'NAME': os.environ.get('DB_NAME', 'ddshoes'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'root'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
